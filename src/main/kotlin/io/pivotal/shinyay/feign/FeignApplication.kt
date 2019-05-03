709d9aac4403b6ff029f9b7af6e017e0839812ad
shinyay <shinya.com@gmail.com>
2019-05-03 14:31:37 +0900

    Initial Commit

---
+++ src/main/kotlin/io/pivotal/shinyay/feign/FeignApplication.kt
@@ -0,0 +1,11 @@
+package io.pivotal.shinyay.feign
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeignApplication
+
+fun main(args: Array<String>) {
+	runApplication<FeignApplication>(*args)
+}
