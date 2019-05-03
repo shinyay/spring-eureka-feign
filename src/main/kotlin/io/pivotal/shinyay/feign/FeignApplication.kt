a0d94e587dfb5b4b45acabc01ab60d6098c2b001
shinyay <shinya.com@gmail.com>
2019-05-03 15:34:20 +0900

    Add restcontroller with feign client

--- src/main/kotlin/io/pivotal/shinyay/feign/FeignApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/feign/FeignApplication.kt
@@ -2,9 +2,22 @@ package io.pivotal.shinyay.feign
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
 
 @SpringBootApplication
-class FeignApplication
+@EnableFeignClients
+@Controller
+class FeignApplication(val greetingInterface: GreetingInterface) {
+
+	@RequestMapping("/greeting")
+	fun greeting(model: Model):String {
+		model.addAttribute("greeting", greetingInterface.greeting())
+		return "greeting-view"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<FeignApplication>(*args)
